99b5181c59bb4b7f479afe5a0468fdb44169e6cd
Gaurav Kumar <103420886+gauravai2025@users.noreply.github.com>
2022-12-19 13:44:07 +0530

    Add files via upload

---
+++ function/sum of two integer by function.cpp
@@ -0,0 +1,19 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int sum1(int a,int b){
+int s=a+b;
+return s;}
+
+int main() {
+    // Write C++ code here
+    int a,b;
+    cout<<"enter first and second number";
+    cin>>a>>b;
+     int k=sum1(a,b);
+     cout<<k;
+        
+
+
+    return 0;
+}
---
+++ function/sum of two integers by function by void method.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+ void func(int a, int b){
+    cout << (a + b);
+    }
+
+    int main() {
+  
+    func(7, 12);
+    }
