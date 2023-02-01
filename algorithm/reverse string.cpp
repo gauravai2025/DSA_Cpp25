2585dcd2cf78afd3f2f12b57287c879ef94077e7
Gaurav Kumar <103420886+gauravai2025@users.noreply.github.com>
2023-02-02 00:01:57 +0530

    Add files via upload

---
+++ algorithm/reverse string.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout<<"enter string\n";
+    string str;
+    cin>>str;
+    reverse(str.begin(),str.end());
+    cout<<"reversed string: "<<str;
+    return 0;
+}
