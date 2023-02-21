f8a7d9e8f325b7ad0aaf591cbcb7901447a86fc4
Gaurav Kumar <103420886+gauravai2025@users.noreply.github.com>
2023-02-21 16:35:11 +0530

    Add files via upload

---
+++ function/demo1.cpp
@@ -0,0 +1,16 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ void  rev(int size){
+    cout<<size<<endl;
+  }
+int main() {
+cout<<"enter number\n";
+  int n;
+   cin>>n;
+ rev(n++);
+cout<<"number: "<<n;
+
+    return 0;
+}
