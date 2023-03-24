32816671e440ea43062d66d2567dbb8cdb4e1ff3
Gaurav Kumar <103420886+gauravai2025@users.noreply.github.com>
2023-03-24 10:06:29 +0530

    Update sortarray.cpp
    update code

--- algorithm/sortarray.cpp
+++ algorithm/sortarray.cpp
@@ -1,15 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main() {
-    cout<<"enter size of array\n";
-    int size ;
-    cin>>size ;
-    int *arr=new int[size ];
-    cout<<"enter element of array/n";
-    for(int i=0;i<size ;i++){
-        cin>>arr[i];
-        }
-    
-     sort(arr,arr+size);
+ 
+int main()
+{
+    cout<<"enter number of element in array\n";
+int n;
+cin>>n;
+int *a=new int [n]; // dynamically array
+cout<<"enter  element of array\n";
+for(int i=0;i<n;i++){
+    cin>>a[i];
+}
+sort(a,a+n); // sorting whole array
+
+for(int i=0;i<n;i++){
+    cout<<a[i]<<" ";
+}
+
+sort(a,a+3); // sorting from 1st element to 3rd element
+
+
+for(int i=0;i<n;i++){
+    cout<<a[i]<<" ";
+}
+
+sort(a+2,a+n); // sorting fom 3rd element to last element
+
+
+for(int i=0;i<n;i++){
+    cout<<a[i]<<" ";
+}
+
     return 0;
 }
